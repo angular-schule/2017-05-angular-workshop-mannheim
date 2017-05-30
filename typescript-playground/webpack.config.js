01349715e0e6d63e7e119b1e4252d902f5953793
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-30 11:55:16 +0200

    webpack.optimize.UglifyJsPlugin

--- typescript-playground/webpack.config.js
+++ typescript-playground/webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack'); //to access built-in plugins
+
 module.exports = {
     entry: './app.ts',
     output: { filename: 'bundle.js' },
@@ -11,5 +13,9 @@ module.exports = {
                 use: [{ loader: "ts-loader" }]
             }
         ]
-    }
+    },
+    devtool: "source-map",
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ beautify: false, sourceMap: true })
+    ]
 }
