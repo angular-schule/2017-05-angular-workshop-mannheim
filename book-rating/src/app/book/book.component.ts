894559d62fb2c97d5b8a6cfcec19e42bf2f4810a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-30 14:52:27 +0200

    ngFor BookComponent

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,20 @@
+import { Book } from './../shared/book';
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  @Input()
+  book: Book;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
