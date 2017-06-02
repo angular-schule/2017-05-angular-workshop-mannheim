a2334e2b02a46fd69562f555d4a0a8b8c86bcab9
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-06-02 23:12:10 +0200

    fixed reducer for with seamless-immutable

--- book-rating/src/_reducers/names.reducer.spec.ts
+++ book-rating/src/_reducers/names.reducer.spec.ts
@@ -1,14 +1,19 @@
-let myReducer = (acc, name) => {
+import * as si from 'seamless-immutable';
+
+const myReducer = (acc, name) => {
   const count = acc[name];
-  acc[name] = count ? count + 1 : 1;
-  return acc;
+  // VORHER: acc[name] = count ? count + 1 : 1;
+  const newAcc = acc.set(name, count ? count + 1 : 1);
+  return newAcc;
 };
 
 describe('names reducer', () => {
   it('should count names', () => {
 
     const names = ['Karl', 'Micha', 'Anita', 'Alex', 'Karl', 'Micha'];
-    const actual = names.reduce(myReducer, {});
+
+    const immutableAccumulator = si.from({});
+    const actual = names.reduce(myReducer, immutableAccumulator);
 
     expect(actual).toEqual({
       Karl: 2,
